0bd06ff59ee045b4825f0771883fcc718100ff45
manoj <manomanu8888@gmail.com>
2018-09-27 01:09:39 -0700

    added a new files

--- create-env.sh
+++ create-env.sh
@@ -2,12 +2,12 @@
 cont_count=$1
 echo "creating $cont_count containers.."
 sleep 2;
-for i in `seq $cont_count`
+for i in `seq $ 1 2 3`
 do
 	echo "=============================="
-    echo "Creating www.gamutkart$i container.."
+    echo "Creating www.bang$i container.."
     sleep 1
-    docker run --name www.gamutkart$i -d -it --rm nageshvkn/gamutkart-img /bin/bash
-    echo "www.gamutkart$i container has been created!"
+    docker run --name www.bang$i -d -it --rm mano8888/batch-1 /bin/bash
+    echo "www.bang$i container has been created!"
 	echo "=============================="
 done
